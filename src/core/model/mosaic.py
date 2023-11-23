334ea3fcf0fc93e34a4258ad3a0a746237fc6f96
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2023-11-23 14:47:57 -0300

    Clipping image to aoi.

--- src/core/model/mosaic.py
+++ src/core/model/mosaic.py
@@ -38,7 +38,7 @@ class Mosaic:
         }
 
         # Stitch tiles into a final image
-        final_image = Image.new("RGB", (total_width, total_height))
+        merged_image = Image.new("RGB", (total_width, total_height))
         for x in range(top_left_quadkey.x, bottom_right_quadkey.x + 1):
             for y in range(top_left_quadkey.y, bottom_right_quadkey.y + 1):
                 tile = sorted_lookup[(x, y)]
@@ -47,40 +47,49 @@ class Mosaic:
                 x_offset = (x - top_left_quadkey.x) * tile_width
                 y_offset = (y - top_left_quadkey.y) * tile_height
 
-                final_image.paste(tile_image, (x_offset, y_offset))
+                merged_image.paste(tile_image, (x_offset, y_offset))
                 tile_image.close()
 
-        # Export output image as tiff
-        final_image_path = os.path.join(download_folder_path, f"{aoi.id}.tiff")
-        final_image.save(final_image_path, quality=100)
-        final_image.close()
-        self.file_path = final_image_path
+        # Export merged image as tiff
+        merged_image_path = os.path.join(download_folder_path, f"{aoi.id}-merged.tiff")
+        merged_image.save(merged_image_path, quality=100)
+        merged_image.close()
+
+        # Open merged image as raster
+        merged_image_raster = gdal.Open(merged_image_path, gdal.GA_Update)
 
         # Get corner coordinates
         upper_left_x, upper_left_y = aoi.envelope[0], aoi.envelope[3]
         lower_right_x, lower_right_y = aoi.envelope[1], aoi.envelope[2]
 
-        # Open output image as raster
-        final_image_raster = gdal.Open(final_image_path, gdal.GA_ReadOnly)
-
         # Create a new transformation
         geo_transform = [
             upper_left_x,
-            (lower_right_x - upper_left_x) / final_image_raster.RasterXSize,
+            (lower_right_x - upper_left_x) / merged_image_raster.RasterXSize,
             0,
             upper_left_y,
             0,
-            (lower_right_y - upper_left_y) / final_image_raster.RasterYSize,
+            (lower_right_y - upper_left_y) / merged_image_raster.RasterYSize,
         ]
 
-        # Open the image file in write mode
-        final_image_raster = gdal.Open(final_image_path, gdal.GA_Update)
-
-        # Set the new geotransform and projection
+        # Georeference merged image
         spatial_reference = osr.SpatialReference()
         spatial_reference.ImportFromEPSG(4326)
-        final_image_raster.SetGeoTransform(geo_transform)
-        final_image_raster.SetProjection(spatial_reference.ExportToWkt())
+        merged_image_raster.SetGeoTransform(geo_transform)
+        merged_image_raster.SetProjection(spatial_reference.ExportToWkt())
+
+        # Clip image to AOI
+        clipped_image_path = os.path.join(
+            download_folder_path, f"{aoi.id}-clipped.tiff"
+        )
+        gdal.Translate(
+            clipped_image_path,
+            merged_image_raster,
+            projWin=[upper_left_x, upper_left_y, lower_right_x, lower_right_y],
+        )
 
         # Close driver
-        final_image_raster = None
+        merged_image_raster = None
+
+        # Set mosaic image property
+        self.file_path = clipped_image_path
